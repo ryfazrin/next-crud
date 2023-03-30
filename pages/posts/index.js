b8ce572ecdb8164aba42170e1a7d0980ced675a8
ryfazrin <riansera515@gmail.com>
2023-03-30 13:09:41 +0700

    created: posts page

---
+++ pages/posts/index.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+// fetch with "getServerSideProps"
+export async function getServerSideProps() {
+  // http request
+  const req = await axios.get(`${process.env.NEXT_PUBLIC_API_BACKEND}/api/posts`)
+  const res = await req.data.data.data
+
+  return {
+    props: {
+      posts: res // <-- assign response
+    }
+  }
+}
+
+function PostIndex(props) {
+
+  //destruct
+  const { posts } = props;
+
+  return (
+    <Layout>
+      <div className="container" style={{ marginTop: '80px' }}>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card border-0 shadow-sm rounded-3">
+              <div className="card-body">
+                <Link href="/posts/create" legacyBehavior>
+                  <button className="btn btn-primary border-0 shadow-sm mb-3">TAMBAH</button>
+                </Link>
+                <table className="table table-bordered mb-0">
+                  <thead>
+                    <tr>
+                      <th scope="col">IMAGE</th>
+                      <th scope="col">JUDUL</th>
+                      <th scope="col">CONTENT</th>
+                      <th scope="col">AKSI</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {posts.map((post) => (
+                      <tr key={post.id}>
+                        <td className="text-center">
+                          <Image src={`${process.env.NEXT_PUBLIC_API_BACKEND}/storage/posts/${post.image}`} className="rounded-3" width="150" alt="" />
+                        </td>
+                        <td>{post.title}</td>
+                        <td>{post.content}</td>
+                        <td className="text-center">
+                          <button className="btn btn-sm btn-primary border-0 shadow-sm mb-3 me-3">EDIT</button>
+                          <button className="btn btn-sm btn-danger border-0 shadow-sm mb-3">DELETE</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default PostIndex
